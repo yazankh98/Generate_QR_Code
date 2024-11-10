e56677239822d445e1ab4d72504cd21fe7e5ead1
yazankh98 <yazan.kh.anam@gmail.com>
2024-11-10 17:14:50 +0400

    Qr

--- assets/websit.js
+++ assets/websit.js
@@ -1,11 +1,11 @@
-function generateQRCode() {
-    const url = document.getElementById("url-input").value;
-    if (url) {
-        // Clear previous QR code (if any)
-        document.getElementById("qrcode").innerHTML = "";
-        // Generate new QR code
-        new QRCode(document.getElementById("qrcode"), url);
-    } else {
-        alert("Please enter a URL.");
-    }
-}
+// function generateQRCode() {
+//     const url = document.getElementById("url-input").value;
+//     if (url) {
+//         // Clear previous QR code (if any)
+//         document.getElementById("qrcode").innerHTML = "";
+//         // Generate new QR code
+//         new QRCode(document.getElementById("qrcode"), url);
+//     } else {
+//         alert("Please enter a URL.");
+//     }
+// }
--- assets/whatsupp.js
+++ assets/whatsupp.js
@@ -1,15 +1,15 @@
-function generateQRCode() {
-    const phoneNumber = document.getElementById("phone-number").value;
-    if (phoneNumber) {
-        // WhatsApp API URL format
-        const whatsappURL = `https://wa.me/${phoneNumber}`;
+// function generateQRCode() {
+//     const phoneNumber = document.getElementById("phone-number").value;
+//     if (phoneNumber) {
+//         // WhatsApp API URL format
+//         const whatsappURL = `https://wa.me/${phoneNumber}`;
 
-        // Clear previous QR code (if any)
-        document.getElementById("qrcode").innerHTML = "";
+//         // Clear previous QR code (if any)
+//         document.getElementById("qrcode").innerHTML = "";
 
-        // Generate new QR code
-        new QRCode(document.getElementById("qrcode"), whatsappURL);
-    } else {
-        alert("Please enter a phone number.");
-    }
-}
+//         // Generate new QR code
+//         new QRCode(document.getElementById("qrcode"), whatsappURL);
+//     } else {
+//         alert("Please enter a phone number.");
+//     }
+// }
